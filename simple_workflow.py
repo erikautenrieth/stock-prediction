6239940b294eeb4deddff484f1abf2b34710c030
erik <e-aut@web.de>
2023-11-17 14:35:55 +0100

    add mlflow models

--- simple_workflow.py
+++ simple_workflow.py
@@ -7,4 +7,5 @@ df.set_index('Date', inplace=True)
 DEFAULT_MODEL = "runs:/5c036be77ea045228b58b4fa52821f65/model"
 loaded_model = mlflow.pyfunc.load_model(DEFAULT_MODEL)
 prediction = loaded_model.predict(df)
+
 print(prediction)
