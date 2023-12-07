5fb0df15176fb85b26c72ce618970c2dab6fe777
erik <e-aut@web.de>
2023-12-07 21:04:08 +0100

    - update ui

--- frontend/charts.py
+++ frontend/charts.py
@@ -1,7 +1,47 @@
-from pygments.lexers import go
+import plotly.graph_objs as go
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 import yfinance as yf
+
+
+def candlestick_chart(data, predictions, stock_symbol):
+    fig = go.Figure(data=[go.Candlestick(x=data.index,
+                                         open=data['Open'],
+                                         high=data['High'],
+                                         low=data['Low'],
+                                         close=data['Close'])])
+
+    for index, row in predictions.iterrows():
+        date = row['Date']
+        target = row['Target']
+        if date in data.index:
+            fig.add_annotation(
+                x=date,
+                y=data.loc[date, 'Close'],
+                text='↑' if target == 1 else '↓',
+                showarrow=True,
+                arrowhead=2,
+                arrowsize=1,
+                arrowwidth=2,
+                arrowcolor='green' if target == 1 else 'red'
+            )
+
+    fig.update_layout(
+        title=f'Stock Prediction for {stock_symbol[1]}',
+        xaxis_rangeslider_visible=False,
+        xaxis=dict(
+            rangeselector=dict(
+                buttons=list([
+                    dict(count=1, label='1M', step='month', stepmode='backward'),
+                    dict(count=3, label='3M', step='month', stepmode='backward'),
+                    dict(step='all')
+                ])
+            ),
+            type='date'
+        ),
+        xaxis_tickformat='%Y-%m-%d'
+    )
+    return fig
 
 
 def line_chart(predictions, stock_symbol):
@@ -28,7 +68,7 @@ def line_chart(predictions, stock_symbol):
                 arrowcolor='green' if target == 1 else 'red'
             )
         fig_linie.update_layout(
-            title=f'3-Month Line Chart for {stock_symbol[1]}',
+            title=f'3-Month Chart for {stock_symbol[1]}',
             xaxis=dict(
                 type='date'
             ),
--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -1,13 +1,12 @@
 import streamlit as st
 import yfinance as yf
-import plotly.graph_objs as go
 import pandas as pd
 import os
 import sys
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
-from charts import line_chart
+from charts import line_chart, candlestick_chart
 
 base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if base_dir not in sys.path:
@@ -16,6 +15,8 @@ if base_dir not in sys.path:
 from ml.database.influxdb_manager import InfluxDBOperations
 
 db = InfluxDBOperations()
+
+# streamlit run streamlit_app.py
 
 
 def main():
@@ -33,49 +34,14 @@ def main():
     if not data.empty:
         predictions = db.get_prediction_from_influx()
 
-        # Kerzenchart erstellen
-        fig = go.Figure(data=[go.Candlestick(x=data.index,
-                                             open=data['Open'],
-                                             high=data['High'],
-                                             low=data['Low'],
-                                             close=data['Close'])])
-
-        for index, row in predictions.iterrows():
-            date = row['Date']
-            target = row['Target']
-            if date in data.index:
-                fig.add_annotation(
-                    x=date,
-                    y=data.loc[date, 'Close'],
-                    text='↑' if target == 1 else '↓',
-                    showarrow=True,
-                    arrowhead=2,
-                    arrowsize=1,
-                    arrowwidth=2,
-                    arrowcolor='green' if target == 1 else 'red'
-                )
-
-        fig.update_layout(
-            title=f'Stock Prediction for {stock_symbol[1]}',
-            xaxis_rangeslider_visible=False,
-            xaxis=dict(
-                rangeselector=dict(
-                    buttons=list([
-                        dict(count=1, label='1M', step='month', stepmode='backward'),
-                        dict(count=3, label='3M', step='month', stepmode='backward'),
-                        dict(step='all')
-                    ])
-                ),
-                type='date'
-            ),
-            xaxis_tickformat='%Y-%m-%d'
-        )
-        st.plotly_chart(fig)
-
+        candle_stick_plot = candlestick_chart(data, predictions, stock_symbol)
         line_chart_plot = line_chart(predictions, stock_symbol)
 
+        # Plot the charts
+        st.plotly_chart(candle_stick_plot)
         st.plotly_chart(line_chart_plot)
 
+        # Table with model information
         model_info = predictions.drop("time", axis=1).drop_duplicates()
         model_info['Date'] = pd.to_datetime(model_info['Date']).dt.strftime('%Y-%m-%d')
         st.write("Model Information and Accuracy")
